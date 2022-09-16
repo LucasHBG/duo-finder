ce2f00aabd1e13ff63da00fb29d732053c16d549
Lucas Guimarães <lukahbg@gmail.com>
2022-09-16 00:38:09 -0300

    [feat]: added background image and logo to the page and added custom styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,19 @@
-import './styles/main.css';
+import "./styles/main.css";
+
+import logoImg from "./assets/logo-nlw.svg";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="NLW eSports" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
     </div>
   );
 }
