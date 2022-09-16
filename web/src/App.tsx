6e325ecdbe298aeb9d5aced0c71335a6862d5f1d
Lucas Guimarães <lukahbg@gmail.com>
2022-09-15 23:50:06 -0300

    [feat]: added and setup tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,8 @@
-interface buttonProps {
-  title: string;
-}
-
-function Button(props: buttonProps) {
-  return <button>{props.title}</button>;
-}
+import './styles/main.css';
 
 function App() {
   return (
     <div>
-      <h1> Hello world </h1>
-      <Button title="btn 1"/>
-      <Button title="btn 2"/>
-      <Button title="Gol?"/>
     </div>
   );
 }
