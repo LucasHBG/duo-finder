8b2816aae72ec816b9fda19a9622666a1b380d6d
Lucas Guimarães <lukahbg@gmail.com>
2022-09-16 23:32:50 -0300

    [feat]: set Inter as default font-family

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -3,11 +3,16 @@ module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
-      backgroundImage:{
-        'nlw-galaxy': "url('/background-nlw-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
-        'nlw-game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
-      }
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "nlw-galaxy": "url('/background-nlw-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)",
+        "nlw-game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+      },
     },
   },
   plugins: [],
