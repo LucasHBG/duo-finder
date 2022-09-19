264f9dfd9d64a04cdd451f6a5b9657cda77664c8
Lucas Guimarães <lukahbg@gmail.com>
2022-09-19 11:38:23 -0300

    [feat]: set default splash and background images, and app theme color background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Background } from "./src/components/background";
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="OI" />
-      <Button title="kc" />
-      <Button title="mendigo do blues" />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
