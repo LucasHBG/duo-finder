103410e33470627530fd178c62f889b7733b0eeb
Lucas Guimarães <lukahbg@gmail.com>
2022-10-13 23:56:01 -0300

    [feat]: Added endpoint to list ads from a game ordered by most recent Ad creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,26 +10,26 @@ const app = express();
  * Configuration variable for Prisma
  */
 const prisma: PrismaClient = new PrismaClient({
-  log: ['query']
+    log: ["query"],
 });
 
 /**
- * Get list of all games from database and add "count" column 
+ * Get list of all games from database and add "count" column
  * to show how many ads a game has
  *
  * @return list of games + count column
  */
-app.get("/games", async (request, response)  => {
-  const games = await prisma.game.findMany({
-    include: {
-      _count: {
-        select:{
-          ads: true,
-        }
-      }
-    }
-  });
-  return response.json(games);
+app.get("/games", async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                },
+            },
+        },
+    });
+    return response.json(games);
 });
 
 /**
@@ -38,7 +38,7 @@ app.get("/games", async (request, response)  => {
  * @return Http status code
  */
 app.post("/ads", (request, response) => {
-  return response.status(201).json([]);
+    return response.status(201).json([]);
 });
 
 /**
@@ -47,14 +47,14 @@ app.post("/ads", (request, response) => {
  * @return list with all ads
  */
 app.get("/ads", (request, response) => {
-  return response.json([
-    { id: 1, description: "Anúncio 1" },
-    { id: 2, description: "Anúncio 2" },
-    { id: 3, description: "Anúncio 3" },
-    { id: 4, description: "Anúncio 4" },
-    { id: 5, description: "Anúncio 5" },
-    { id: 6, description: "Anúncio 6" },
-  ]);
+    return response.json([
+        { id: 1, description: "Anúncio 1" },
+        { id: 2, description: "Anúncio 2" },
+        { id: 3, description: "Anúncio 3" },
+        { id: 4, description: "Anúncio 4" },
+        { id: 5, description: "Anúncio 5" },
+        { id: 6, description: "Anúncio 6" },
+    ]);
 });
 
 /**
@@ -63,18 +63,31 @@ app.get("/ads", (request, response) => {
  * @return list of ads from one single game
  */
 app.get("/games/:id/ads", (request, response) => {
-  return response.json([
-    { id: 1, description: "Anúncio 1" },
-    { id: 2, description: "Anúncio 2" },
-    { id: 3, description: "Anúncio 3" },
-    { id: 4, description: "Anúncio 4" },
-    { id: 5, description: "Anúncio 5" },
-    { id: 6, description: "Anúncio 6" },
-  ]);
+    const gameId = request.params.id;
+
+    const ads = new PrismaClient().ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            yearsPlaying: true,
+            useVoiceChannel: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId: gameId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    return response.json(ads);
 });
 
 app.get("ads/:id/discord", (request, response) => {
-  return response.json([]);
+    return response.json([]);
 });
 
 app.listen(3333);
